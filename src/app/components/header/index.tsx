808586588fb98f78860505109fd2c5d29dd95bfe
Nihat Çetinkaya <nihat19900991@gmail.com>
2025-03-15 23:52:43 +0300

    first commit

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react";
+import Input from "./input";
+import { BiSolidBellRing } from "react-icons/bi";
+import man from "../../assets/images/users.webp";
+import Image from "next/image";
+
+const Header: FC = () => {
+  return (
+    <header className="border-b border-zinc-300 p-2 bg-white flex justify-between">
+      <Input />
+
+      <div className="flex gap-5 items-center">
+        <BiSolidBellRing className="text-xl cursor-pointer" />
+
+        <div className="flex gap-3">
+          <Image
+            src={man}
+            width={50}
+            height={50}
+            alt="profile"
+            className="size-12 rounded-full"
+          />
+
+          <div>
+            <p className="font-semibold">Metin DOĞRU</p>
+            <p>Admin</p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
