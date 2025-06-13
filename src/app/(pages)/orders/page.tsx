26c8e0a65934a2bca62656d0e27e81026e9ecbb5
nihatctnky <nihat19900991@gmail.com>
2025-06-13 17:22:41 +0300

    İlk kendi commitim

---
+++ src/app/(pages)/orders/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import OrderTable from "@/app/components/table/order-table";
+
+const Orders: FC = () => {
+  return (
+    <div className="page">
+      <h1 className="title">Orders</h1>
+      <OrderTable />
+    </div>
+  );
+};
+
+export default Orders;
---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,62 @@
+import { FC } from "react";
+import icon1 from "@/app/assets/images/icon-1.webp";
+import icon2 from "@/app/assets/images/icon-2.webp";
+import icon3 from "@/app/assets/images/icon-3.webp";
+import icon4 from "@/app/assets/images/icon-4.png";
+import { InfoItem } from "../types";
+import InfoCard from "../components/card/info-card";
+import OrderContainer from "../components/graphic/order-container";
+import CategoryContainer from "../components/graphic/category-container";
+import { getValues } from "../utils/service";
+
+const App: FC = async () => {
+  const values = await getValues();
+
+  // *Ekrana basılacak kart dizisi
+  const cards: InfoItem[] = [
+    {
+      icon: icon1,
+      label: "Toplam Kullanıcı",
+      value: values.total_users * 223,
+    },
+    {
+      icon: icon2,
+      label: "Toplam Sipariş",
+      value: values.total_orders * 339,
+    },
+    {
+      icon: icon3,
+      label: "Toplam Satış",
+      value: (values.total_price * 26).toLocaleString() + "$",
+    },
+    {
+      icon: icon4,
+      label: "Ürün Sayısı",
+      value: values.total_products * 423,
+    },
+  ];
+
+  return (
+    <div className="page">
+      <h1 className="title">Admin Paneli</h1>
+
+      <section className="grid sm:grid-cols-2 lg:grid-cols-4 gap-5 mt-5">
+        {cards.map((item, key) => (
+          <InfoCard key={key} item={item} />
+        ))}
+      </section>
+
+      <section className="grid lg:grid-cols-3 gap-5 mt-5">
+        <div className="lg:col-span-2">
+          <OrderContainer />
+        </div>
+
+        <div className="lg:col-span-1">
+          <CategoryContainer />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default App;
